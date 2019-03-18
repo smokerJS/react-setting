5852b0bcadfb95258176404c4902cb219b3fb310
“Jung <smokerjs@stunitas.com>
2019-03-18 19:08:51 +0900

    react 초기 셋팅

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import { AppContainer } from 'react-hot-loader';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App';
+
+const render = Component =>
+  ReactDOM.render(
+    <AppContainer>
+      <Component />
+    </AppContainer>,
+    document.getElementById('root')
+  );
+
+render(App);
+
+if (module.hot) module.hot.accept('./components/App', () => render(App));
